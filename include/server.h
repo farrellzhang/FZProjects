f7f7c27764ac394cef3912d7892d5e6fa3c776f3
farrellzhang <rtbh90@163.com>
2017-12-30 14:05:00 +0800

    Test

---
+++ include/server.h
@@ -0,0 +1,16 @@
+//服务类
+class FileServer
+{
+    public:
+	FileServer();
+	~FileServer();
+	void start();
+    protected:
+	//void init();
+	//void loop();
+	//static void* server_fun(void* param);
+    protected:
+	//int _sk;
+	//int _efd;
+	//pthread_t _tid;
+};
